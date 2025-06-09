0c57fdbad329f35b2f8aa8c928dec37e7794a3a6
reon1do <faiteforever@gmail.com>
2025-06-09 18:48:58 +0300

    Updated config

--- be/src/config/index.ts
+++ be/src/config/index.ts
@@ -10,17 +10,7 @@ class Config {
   public readonly env: string = process.env.NODE_ENV ?? "development";
 
   /* Database */
-  private readonly dbHost: string = process.env.DB_HOST ?? "localhost";
-  private readonly dbUser: string = process.env.POSTGRES_USER!;
-  private readonly dbPassword: string = process.env.POSTGRES_PASSWORD!;
-  private readonly dbName: string = process.env.POSTGRES_DB!;
-  private readonly dbPort: number = Number(process.env.POSTGRES_PORT!);
-  public readonly dbUrl: string = [
-    "postgresql://",
-    `${this.dbUser}:${this.dbPassword}@`,
-    `${this.dbHost}:${this.dbPort}/`,
-    this.dbName,
-  ].join("");
+  public readonly dbUrl: string = process.env.DB_URL!;
 }
 
 export default new Config();
